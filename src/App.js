1917fd4baff63639cd9c08fb61f4dc9206e9b783
preetamrawat <psrawat1320@gmail.com>
2021-04-12 15:39:19 +0530

    displayed draq message with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import Board from "./components/Board";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 import { calculateWinner } from "./helpers";
 import "./styles/root.scss";
 
@@ -17,7 +18,6 @@ const winner = calculateWinner(current.board);
 
 
 
-const message= winner ? `winner is ${winner}` :   `Next player is ${current.isXNext ? 'X' :'O'}`;
 
   const handleSquareClick=(position)=>{
     if(current.board[position] || winner){
@@ -41,11 +41,11 @@ const message= winner ? `winner is ${winner}` :   `Next player is ${current.isXN
   const moveTo=(move)=>{
      setCurrentMove(move); 
   }
-
+ 
   return(
     <div className="app">
     <h1>TIC TAC TOE</h1>
-    <h2>{message} </h2>
+    <StatusMessage winner={winner} current={current}/>
     <Board board={current.board} handleSquareClick={handleSquareClick} />
     <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
